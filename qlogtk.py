3494d8333b92357e4fe1af122e2dc6dd4dff6321
Mike Vandersanden <mike.vandersanden@uhasselt.be>
2023-01-11 10:05:11 +0100

    add received

--- qlogtk.py
+++ qlogtk.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import json
 
 sent_packets = {}
+received_packets = {}
 
 with open('test.qlog', 'r') as file:
     header = file.readline()
@@ -10,15 +11,35 @@ with open('test.qlog', 'r') as file:
         category, event_type = data['name'].split(':')
 
         if category == 'transport' \
-        and event_type == 'packet_sent':
+        and event_type == 'packet_received':
+
             packet_number = data['data']['header']['packet_number']
             if not packet_number in sent_packets:
                 sent_packets[packet_number] = {}
             sent_packets[packet_number]['sent'] = data['time']
             sent_packets[packet_number]['size'] = data['data']['raw']['length']
         
+            for frame in data['data']['frames']:
+                if frame['frame_type'] == 'ack':
+                    for ack_range in frame['acked_ranges']:
+                        print(ack_range)
+                        if len(ack_range) == 1:
+                            received_packets[ack_range[0]]['ack'] = data['time']
+                        else:
+                            for packet_number in range(ack_range[0], ack_range[1]+1):
+                                print(ack_range[0], packet_number, ack_range[1])
+                                received_packets[packet_number]['ack'] = data['time']
+                    #TODO use ack_delay?
+        
         elif category == 'transport' \
-        and event_type == 'packet_received':
+        and event_type == 'packet_sent':
+
+            packet_number = data['data']['header']['packet_number']
+            if not packet_number in received_packets:
+                received_packets[packet_number] = {}
+            received_packets[packet_number]['sent'] = data['time']
+            received_packets[packet_number]['size'] = data['data']['raw']['length']
+
             for frame in data['data']['frames']:
                 if frame['frame_type'] == 'ack':
                     for ack_range in frame['acked_ranges']:
@@ -31,24 +52,54 @@ with open('test.qlog', 'r') as file:
                                 sent_packets[packet_number]['ack'] = data['time']
                     #TODO use ack_delay?
 
-x_axis = []
-y_axis = []
 
-for packet_number in sent_packets:
-    instantaneous_throughput = 0
+sent_x_axis = []
+sent_y_axis = []
+received_x_axis = []
+received_y_axis = []
 
-    data = sent_packets[packet_number]
+sent_y_avg = []
+received_y_avg = []
+alpha = 0.5
 
-    if 'size' in data and 'sent' in data and 'ack' in data:
-        instantaneous_throughput = data['size'] / (data['ack'] - data['sent'])
-    else:
-        pass
-        print(packet_number)
-     
-    x_axis.append(packet_number)
-    y_axis.append(instantaneous_throughput)
+for g in [[sent_packets, sent_x_axis, sent_y_axis, sent_y_avg],[received_packets, received_x_axis, received_y_axis, received_y_avg]]:
+    packets = g[0]
+    x_axis = g[1]
+    y_axis = g[2]
+    y_avg = g[3]
+    c_avg = None
+    for packet_number in packets:
+        instantaneous_throughput = 0
 
-plt.plot(x_axis, y_axis, 'bs')
-plt.ylabel('instantaneous throughput')
+        data = packets[packet_number]
+
+        if 'size' in data and 'sent' in data and 'ack' in data:
+            instantaneous_throughput = ((data['size']*8) / ((data['ack'] - data['sent'])/1000))/1000
+        else:
+            pass
+            print(packet_number)
+        
+        x_axis.append(packet_number)
+        y_axis.append(instantaneous_throughput)
+
+        if c_avg == None:
+            c_avg = instantaneous_throughput
+        else:
+            c_avg = ((1-alpha) * c_avg) + (alpha * instantaneous_throughput)
+        y_avg.append(c_avg)
+
+plt.figure(figsize=(14, 5))
+plt.suptitle('packet throughput')
+plt.subplot(121)
+plt.title("sent")
+plt.plot(sent_x_axis, sent_y_axis, 'bs', markersize=1.5)
+plt.plot(sent_x_axis, sent_y_avg, 'r')
+plt.ylabel('instantaneous throughput (Kbps)')
+plt.xlabel('packetnumber')
+plt.subplot(122)
+plt.title("received")
+plt.plot(received_x_axis, received_y_axis, 'bs', markersize=1.5)
+plt.plot(received_x_axis, received_y_avg, 'r')
+plt.ylabel('instantaneous throughput (Kbps)')
 plt.xlabel('packetnumber')
 plt.show()
